b03c16b45803f57e87a3b512b083f4fa36e7f513
Erland Lindmark <erland@isaksson.info>
2025-11-15 07:18:39 +0100

    Updated so basepath is handle properly, required for correct urls on GitHub pages

--- src/main.tsx
+++ src/main.tsx
@@ -17,9 +17,11 @@ initAppSettings();
 void initializeBeadStoreFromStorage();
 initializeAutoSave();
 
+const basename = import.meta.env.BASE_URL;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
